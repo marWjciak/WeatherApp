7ae5b351b9f5d3ecdf1a0828657c98555b6d7d33
Marcin <wojciak.marcin@gmail.com>
2019-12-24 16:11:51 +0100

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Marcin Wójciak on 24/12/2019.
+//  Copyright © 2019 Marcin Wójciak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
