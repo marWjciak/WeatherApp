35bcecb6df03b0498a93caa47838db648ddef79b
Marcin <wojciak.marcin@gmail.com>
2020-01-17 22:30:39 +0100

    Add current location weather cell

---
+++ WeatherApp/View/LocationWeatherCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocationWeatherCell.swift
+//  WeatherApp
+//
+//  Created by Marcin Wójciak on 17/01/2020.
+//  Copyright © 2020 Marcin Wójciak. All rights reserved.
+//
+
+import UIKit
+
+class LocationWeatherCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
